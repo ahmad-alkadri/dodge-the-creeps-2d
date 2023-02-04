6935cbf460f60848ff954578bad5dc5eaaa84dc4
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-02-05 00:05:10 +0100

    Connecting HUD to the main and mob scenes

--- Main.gd
+++ Main.gd
@@ -6,14 +6,20 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
+	get_tree().call_group("mobs",
+		"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -46,6 +52,4 @@ func _on_MobTimer_timeout():
 
 func _ready():
 	randomize()
-	# Uncomment line below if you want to test
-	# new_game()
 
